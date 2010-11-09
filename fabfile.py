a44fe407601f3dcbf9b8ca5a700a196aaa337137
Eric Holscher <eric@ericholscher.com>
2010-11-08 19:59:21 -0600

    Make non-chef-version-dependant bin path.

--- fabfile.py
+++ fabfile.py
@@ -2,12 +2,13 @@ from fabric.api import env, local, sudo
 env.user = 'root'
 env.hosts = ['204.232.205.196']
 
-env.chef_executable = '/var/lib/gems/1.8/gems/chef-0.9.12/bin/chef-solo'
+env.chef_executable = '/var/lib/gems/1.8/gems/bin/chef-solo'
+
 
 def install_chef():
     sudo('apt-get update', pty=True)
     sudo('apt-get install -y git-core rubygems ruby ruby-dev', pty=True)
-    sudo('gem install chef --version "= 0.9.12"', pty=True)
+    sudo('gem install chef', pty=True)
 
 def sync_config():
     local('rsync -av . %s@%s:/etc/chef' % (env.user, env.hosts[0]))
