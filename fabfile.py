21a73b5ca086ae9b6343832a273ff95a27a9036c
Eric Holscher <eric@ericholscher.com>
2010-11-10 18:53:12 -0600

    Some fabfile updates for gunicorn.

--- fabfile.py
+++ fabfile.py
@@ -2,6 +2,10 @@ from fabric.api import env, local, sudo
 env.user = 'root'
 env.hosts = ['204.232.205.196']
 
+env.code_dir = '/home/docs/sites/readthedocs.org/checkouts/readthedocs.org'
+env.virtualenv = '/home/docs/sites/readthedocs.org'
+env.rundir = '/home/docs/sites/readthedocs.org/run'
+
 env.chef_executable = '/var/lib/gems/1.8/bin/chef-solo'
 
 
@@ -16,3 +20,12 @@ def sync_config():
 def update():
     sync_config()
     sudo('cd /etc/chef && %s' % env.chef_executable, pty=True)
+
+def reload():
+    "Reload the server."
+    env.user = "docs"
+    run("kill -HUP `cat %s/gunicorn.pid`" % env.rundir)
+
+def restart():
+    "Restart (or just start) the server"
+    run('restart readthedocs-gunicorn')
